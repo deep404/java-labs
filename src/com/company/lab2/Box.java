d80a090c8fbe4ddae87062b2ed0416e9faea8669
vmititelu <vitalie.mititelu@gmail.com>
2020-09-21 12:41:01 +0300

    Update box class with parameterized construstor, plus 3 examples

--- src/com/company/lab2/Box.java
+++ src/com/company/lab2/Box.java
@@ -2,8 +2,25 @@ package com.company.lab2;
 
 public class Box {
 
-    double height;
+    double height ;
     double width;
     double depth;
 
+    Box() {
+        height = 1;
+        width = 1;
+        depth = 1;
+    }
+
+    Box(double x) {
+        height = x;
+        width = x;
+        depth = x;
+    }
+
+    Box(double x, double y, double z) {
+        height = x;
+        width = y;
+        depth = z;
+    }
 }
---
+++ src/com/company/lab2/basic.java
@@ -0,0 +1,16 @@
+package com.company.lab2;
+
+public class basic {
+
+    public static void main(String[] args){
+        Box obj1 = new Box();
+
+        Box obj2 = new Box(4);
+
+        Box obj3 = new Box(3,5,6);
+
+//        System.out.println(obj1.depth+" "+obj1.height+" "+obj1.width);
+//        System.out.println(obj2.depth+" "+obj2.height+" "+obj2.width);
+//        System.out.println(obj3.depth+" "+obj3.height+" "+obj3.width);
+    }
+}
